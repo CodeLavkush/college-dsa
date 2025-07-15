4a57547641388836429042b535c1eb27cc933d2b
codelavkush <mauryalavkush19@gmail.com>
2025-07-16 00:18:57 +0530

    first commit

---
+++ 01_program.py
@@ -0,0 +1,54 @@
+
+def bubble_sort(arr):
+    """
+        first loop runs from last index.
+        second loop runs from first index.
+        comparing both elements and switch tham.
+    """
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+
+def main():
+    arr = []
+    n = int(input("Enter number of elements: "))
+    for i in range(n):
+        element = int(input(f"Enter {i}th element: "))
+        arr.append(element)
+        
+    while True:
+        try:
+            print("MAIN MENU".center(20, "*"))
+            print("1.Display")
+            print("2.Search")
+            print("3.Sort")
+            print("4.Reverse")
+            print("5.Exit")
+            choice = int(input("Enter your choice(1-5): "))
+            match choice:
+                case 1:
+                    print(arr)
+                case 2:
+                    element = int(input("Enter value to be searched: "))
+                    index = arr.index(element)
+                    if index is not None:
+                        print(f"Value found at location {index}")
+                    else:
+                        print("Value not found")
+                case 3:
+                    arr = bubble_sort(arr)
+                case 4:
+                    arr.reverse()
+                case 5:
+                    break
+        except Exception as error:
+            print("ERROR:", error)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02_program.py
@@ -0,0 +1,60 @@
+
+def bubble_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = arr[j]
+    return arr
+
+def create():
+    arr = []
+    n = int(input("Enter number of elements: "))
+    for i in range(n):
+        element = int(input(f"Enter {i}th element: "))
+        arr.append(element)
+    return arr
+
+def main():
+    arr1 = []
+    arr2 = []
+    merged_arr = []
+    while True:
+        try:
+            print("MAIN MENU".center(24, "*"))
+            print("1.Create 1st array")
+            print("2.Create 2nd array")
+            print("3.Sort 1st array")
+            print("4.Sort 2nd array")
+            print("5.Merge")
+            print("6.Sorted merged array")
+            print("7.Exit")
+            choice = int(input("Enter your choice(1-7): "))
+            match choice:
+                case 1:
+                    arr1 = create()
+                    print("The 1st array is: ", arr1)
+                case 2:
+                    arr2 = create()
+                    print("The 2nd array is: ", arr2)
+                case 3:
+                    arr1 = bubble_sort(arr1)
+                    print("The sorted 1st array: ", arr1)
+                case 4:
+                    arr2 = bubble_sort(arr2)
+                    print("The sorted 2nd array: ", arr2)
+                case 5:
+                    merged_arr = arr1 + arr2
+                case 6:
+                    print(merged_arr)
+                case 7:
+                    break
+        except Exception as error:
+            print("ERROR:", error)
+
+
+
+
+if __name__ == "__main__":
+    main()
